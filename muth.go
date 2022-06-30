8b7abf1b654ccdec0704bdaa7f55fcaef14db6dd
tigerwill90 <sylvain.muller90@gmail.com>
2022-06-30 23:19:59 +0200

    Do not use a pointer to unsafe.Pointer

--- muth.go
+++ muth.go
@@ -9,26 +9,24 @@ import (
 var _ http.Handler = (*MutH[http.Handler])(nil)
 
 type MutH[T http.Handler] struct {
-	p *unsafe.Pointer
+	p unsafe.Pointer
 }
 
 func Handler[T http.Handler](r T) *MutH[T] {
 	m := &MutH[T]{
-		p: (*unsafe.Pointer)(unsafe.Pointer(new(T))),
+		p: unsafe.Pointer(&r),
 	}
-	m.Update(r)
 	return m
 }
 
-func (h *MutH[T]) Update(r T) {
-	atomic.StorePointer(h.p, unsafe.Pointer(&r))
+func (h *MutH[T]) Update(new T) {
+	atomic.StorePointer(&h.p, unsafe.Pointer(&new))
 }
 
 func (h *MutH[T]) Swap(new T) (old T) {
-	return *(*T)(atomic.SwapPointer(h.p, unsafe.Pointer(&new)))
+	return *(*T)(atomic.SwapPointer(&h.p, unsafe.Pointer(&new)))
 }
 
 func (h *MutH[T]) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	handler := *(*T)(atomic.LoadPointer(h.p))
-	handler.ServeHTTP(w, r)
+	(*(*T)(atomic.LoadPointer(&h.p))).ServeHTTP(w, r)
 }
